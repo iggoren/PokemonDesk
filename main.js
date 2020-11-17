252c813435d8aeb44fd9f57f0031924461023e27
iggoren <entaev@mail.ru>
2020-11-17 21:22:31 +0800

    Домашняя работа 1

---
+++ main.js
@@ -0,0 +1,17 @@
+const firstRow = 'мама мыла раму';
+const secondRow = 'Собака друг человека';
+
+const countRow1 = getRow(firstRow);
+const countRow2 = getRow(secondRow);
+
+function getRow(Row) {
+ let count = 0;
+   for (let i=0; i < Row.length; i++) {
+    if (Row.charAt(i) === "а") {
+        count++;} 
+     }
+     return count;
+}
+ 
+(countRow1 > countRow2)? console.log(firstRow):console.log(secondRow);
+
