78e7c9ab1171a617b307904b099eb4c945fb5a74
iggoren <entaev@mail.ru>
2020-11-28 22:27:37 +0800

    Revert "Merge pull request #4 from iggoren/homework-4"
    This reverts commit 033894e731673d771cebacfa7e73e41ce20d7b2d, reversing
    changes made to ac20b7f0a9dd717f966a0cb455ce00ef49b6c491.

--- main.js
+++ main.js
@@ -31,8 +31,9 @@ const enemy = {
 }
 
 
+
 $btn.addEventListener('click', function()  {
-    //console.log('Kick');
+    //concole.log('Kick');
     character.changeHP(random(20));
     enemy.changeHP(random(20));  
 });
@@ -40,7 +41,7 @@ $btn.addEventListener('click', function()  {
 $btnStrong.addEventListener('click', function()  {
   
     enemy.changeHP(random(30)); 
-    //$btnStrong.disabled = true;   //Отключение кнопки  
+    $btnStrong.disabled = true;   //Отключение кнопки  
 });
 
 
@@ -65,7 +66,7 @@ function renderProgressbarHP(person) {
 }
 function changeHP(count,person) {
     this.hp.current -= count;
-    //console.log(count)
+    console.log(count)
     const log = this === enemy? generateLog(this, character): generateLog(this, enemy);
     const $p = document.createElement('p');
     $p.innerText = log;
@@ -100,28 +101,6 @@ function generateLog (firstPerson, secondPerson){
 
     return logs[random(logs.length) - 1];
 }
-//счетчик кликов
-function counterClick() {
-    let count = 1;
-     return function() {
-       if (count <=6) { 
-        return count++}
-        else $btn.disabled = true;
-    }; 
-}
-$btn.addEventListener('click', function count1() {
-    console.log(counter1());
-  });
 
-$btnStrong.addEventListener('click', function count2() {
-    console.log(counter2());
-  });
-  
-
-let counter1 = counterClick();
-let counter2 = counterClick();
- 
-
-   
 
 init();
